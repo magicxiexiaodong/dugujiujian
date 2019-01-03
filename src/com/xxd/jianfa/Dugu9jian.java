b83de36a6ef0893781675a58d586817c98cebccb
magicxxd <357301570@qq.com>
2019-01-03 23:45:03 +0800

    l am ybq

--- src/com/xxd/jianfa/Dugu9jian.java
+++ src/com/xxd/jianfa/Dugu9jian.java
@@ -3,5 +3,13 @@ package com.xxd.jianfa;
 public class Dugu9jian {
 	public static void main(String[] args) {
 		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
+		System.out.println("¶À¹Â¾Å½£");
 	}
 }
